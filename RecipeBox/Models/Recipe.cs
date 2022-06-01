f8175cdc970feb9393d082e64a3f34ae6860e243
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-31 19:45:38 -0700

    Create and set the models for the project

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,21 @@
+using RecipeBox.Models;
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public Recipe()
+    {
+      this.JoinEntities = new HashSet<RecipeCategory>();
+    }
+    public int RecipeId { get; set; }
+    public string  DishName { get; set;}
+    public string Origin { get; set; }
+    public string Ingredients { get; set; }
+    public string  Instructions { get; set; }
+    public int EstimatedCost { get; set;}
+    public virtual ICollection<RecipeCategory> JoinEntities { get;}
+
+  }
+}
